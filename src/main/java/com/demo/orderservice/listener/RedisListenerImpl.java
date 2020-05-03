72d588bbb3fa6db039d9372b42bccc846f3dd69c
Shubham <sgupta176@sapient.com>
2020-05-03 16:01:24 +0530

    Logger Added in Redis listener

--- src/main/java/com/demo/orderservice/listener/RedisListenerImpl.java
+++ src/main/java/com/demo/orderservice/listener/RedisListenerImpl.java
@@ -1,14 +1,22 @@
 package com.demo.orderservice.listener;
 
 import com.demo.orderservice.beans.Order;
+import com.demo.orderservice.service.OrderService;
 import org.redisson.api.listener.MessageListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * RedisListenerImpl
  */
 public class RedisListenerImpl implements MessageListener<Order> {
+
+    private static Logger log = LoggerFactory.getLogger(OrderService.class);
+
     @Override
     public void onMessage(String s, Order order) {
-        System.out.println("Order Received: " + order.toString());
+
+        log.info("Order Received: " + order.toString());
+
     }
 }
