dc449dc6f723d8b78624c5dd3ad081e579079abc
abhtripa <abhishek.tripathi@publicissapient.com>
2020-05-04 11:14:09 +0530

    swagger enabled

--- src/main/java/com/demo/orderservice/OrderServiceApplication.java
+++ src/main/java/com/demo/orderservice/OrderServiceApplication.java
@@ -5,15 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
+@EnableSwagger2
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
+    
+    @Bean
+	public Docket orderServiceApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.demo.orderservice")).build();
+	}
 
 }
